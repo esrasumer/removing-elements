fb5fdfe93c6dfb2567df4f9e1e3ed7aca643c5ae
esrasumer <esrasumer28@gmail.com>
2022-05-10 19:11:48 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const taskList = document.querySelector('#task-list');
+
+//***REMOVİNG ELEMENTS***//
+
+// 1-
+//taskList.remove();  //----task-list etiketini sildi----//
+
+// 2-
+//taskList.childNodes[7].remove();  //----task-list etiketinin 7. child etiketini sildi----//
+
+//3-
+//taskList.children[3].remove();   //----task-list etiketinin 3. children etiketini sildi----//
+
+//4-
+//taskList.removeChild(taskList.children[3]); //----task-list etiketinin 3. children etiketini sildi----//
+
+//--------removing attribute--------//
+//1-
+//taskList.children[3].removeAttribute('class'); //----task-list etiketinin 3. children etiketinin class attribute'sini sildi----//
+
+console.log(taskList);
